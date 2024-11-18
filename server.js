8639c4dbb9bea1b3c04cb7f5e06354c017a0a2a9
wwwbrunagon <wwwbrunagon@gmail.com>
2024-11-18 19:37:10 -0300

    config server WIP

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.static("uploads"))
 routes(app)
 
-// Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+    console.log("Hello, World!");
 });
+
+app.get("/api", (req, res) => {
+    res.status(200).send("200 - OK")
+})
